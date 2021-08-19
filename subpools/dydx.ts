618d3c07fd43031f6464fd1663d11baaa512f350
cryptickoan <me@cryptickoan.com>
2021-08-19 14:41:04 -0600

    :sparkles: Added dYdX subpool :sparkles:

---
+++ subpools/dydx.ts
@@ -0,0 +1,31 @@
+var ethers = require('ethers')
+var axios = require('axios')
+
+var Caches = require('../cache.ts')
+
+module.exports = class DydxSubpool {
+    provider
+    cache
+
+    constructor(provider) {
+        this.provider = provider
+        this.cache = new Caches({
+            dydxCurrencyApys: 300,
+        });
+    }
+
+    async getCurrencyApys() {
+        return await this.cache.getOrUpdate("dydxCurrencyApys", async function () {
+            const data = ( await axios.get("https://api.dydx.exchange/v1/markets")).data
+            console.log(data)
+            let apyBNs= {};
+
+            for (let i =0; i < data.markets.length; i++) {
+                console.log(typeof data.markets[i].totalSupplyAPR, data.markets[i].totalSupplyAPR * 1e18)
+                apyBNs[data.markets[i].symbol] = ethers.utils.parseUnits(data.markets[i].totalSupplyAPR, 77)
+            }
+            return apyBNs
+        })
+    }
+
+}
