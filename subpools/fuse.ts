d9cd55dee83fcfe9093809f70275822471fc9c12
cryptickoan <me@cryptickoan.com>
2021-08-20 16:18:42 -0600

    :bug: Fixed return value :bug:

--- subpools/fuse.ts
+++ subpools/fuse.ts
@@ -18,9 +18,19 @@ module.exports = class FuseSubpool {
 
     async getCurrencyApy(cTokenAddress) {
         const cToken = new ethers.Contract(cTokenAddress, cErc20DelegateAbi, this.provider);
-        const supplyRatePerBlock = await cToken.callStatic.supplyRatePerBlock();
-        return supplyRatePerBlock
-    }
+        const supplyRatePerBlock = await cToken.supplyRatePerBlock();
+        return ethers.BigNumber.from( 
+            (
+                (Math.pow( 
+                            (
+                                (supplyRatePerBlock / 1e18) * (4 * 60 * 24)
+                            ) + 1
+                        , 365
+                    ) 
+                - 1) 
+            * 1e18).toFixed(0) 
+        )
+    };
 
     async getCurrencyApys() {
         var self = this;
@@ -28,7 +38,7 @@ module.exports = class FuseSubpool {
         return await self.cache.getOrUpdate("currencyApys", async function () {
             let apyBNs = {};
             for (const currencyCode of Object.keys(self.cTokens)) {
-                apyBNs[currencyCode] = await self.getCurrencyApy(self.cTokens[currencyCode]);
+                apyBNs[currencyCode] =  await self.getCurrencyApy(self.cTokens[currencyCode]);
             }
             return apyBNs
         } )
